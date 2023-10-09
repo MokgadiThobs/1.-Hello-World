2c9f1e7b63608c861a1564a5f82b8961bc249fad
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-09 10:31:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+
+            //To freeze the console  so that it doesn't just flash
+            Console.ReadLine();
+        }
+    }
+}
